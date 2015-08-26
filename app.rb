bb3ca4ce0905ff267ff3c6393ade0dcc7f65ed25
lindsay culver & yelena nedelko <student@epicodus.com>
2015-08-26 15:55:06 -0700

    add initial view files and display all categories on index page.

--- app.rb
+++ app.rb
@@ -2,3 +2,16 @@ require("bundler/setup")
 Bundler.require(:default)
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get('/') do
+  @tags = Tag.all()
+  erb(:index)
+end
+
+post('/tags') do
+  name = params.fetch("name")
+  tag = Tag.new({:name => name, :id => nil})
+  tag.save()
+  @tags = Tag.all()
+  erb(:index)
+end
